5404dc58617d5b7e7505f8c274770511c7e46b40
samrunyan5 <samjorzak@gmail.com>
2021-01-25 22:08:01 -0600

    Finalized workshop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+// NEW
+import {
+  storeCurrentUser,
+  clearCurrentUser
+} from '../auth';
 
 import './Header.css';
 
@@ -11,7 +18,7 @@ const Header = ({
 
   useEffect(() => {
     setSelectedUser(userList[0]);
-  }, [userList]);
+  }, [ userList ]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,11 +31,13 @@ const Header = ({
   }
 
   const handleUserLogin = (event) => {
+    storeCurrentUser(selectedUser); // NEW
     setCurrentUser(selectedUser);
   }
 
   const handleUserLogout = (event) => {
     setSelectedUser(userList[0]);
+    clearCurrentUser(); // NEW
     setCurrentUser(null);
   }
 
@@ -40,14 +49,18 @@ const Header = ({
         onSubmit={ handleSubmit } >
         {
           currentUser
-            ? <button onClick={ handleUserLogout }>LOG OUT, { currentUser.username }</button>
-            : <>
+          ? <> 
+              <NavLink to="/posts" activeClassName="current">POSTS</NavLink>
+              <NavLink to="/todos" activeClassName="current">TODOS</NavLink >
+              <button onClick={ handleUserLogout }>LOG OUT, { currentUser.username }</button>
+            </>
+          : <>
               <select onChange={ handleSelectChange }>{
-                userList.map(user => {
+                userList.map(user => (
                   <option key={ user.id } value={ user.id }>
                     { user.username }
                   </option>
-                })
+                ))
               }</select>
               <button onClick={ handleUserLogin }>LOG IN</button>
             </>
